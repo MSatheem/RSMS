aed808162f73a3d7dde1afdb653f4589a304bd6d
MSatheem <satheem2211@yahoo.com>
2024-02-09 02:11:50 +0530

    Getting product details from database with or without id
    Getting product count where supplier id is given.

--- RetailStoreManagementSystem/src/retailStore/Product.java
+++ RetailStoreManagementSystem/src/retailStore/Product.java
@@ -10,11 +10,13 @@ public class Product {
 	private int id;
 	private String name;
 	private int supplierId;
-	private int quantityInStock=0;
+	private int inStock=0;
 	
 	Connection con;
-	
 	//constructors
+	public Product() {}
+	
+	
 	public Product(int id) {
 		this.id = id;
 	}
@@ -30,12 +32,14 @@ public class Product {
 		this.name = name;
 		this.supplierId = supplierId;
 	}
-
 	
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", supplierId=" + supplierId + ", quantityInStock="
-				+ quantityInStock + "]";
+	
+
+	public Product(int id, String name, int supplierId, int quantityInStock) {
+		this.id = id;
+		this.name = name;
+		this.supplierId = supplierId;
+		this.inStock = quantityInStock;
 	}
 
 	//getters
@@ -45,8 +49,8 @@ public class Product {
 	public String getName() {
 		return name;
 	}
-	public int getQuantityInStock() {
-		return quantityInStock;
+	public int getInStock() {
+		return inStock;
 	}
 	public int getSupplierId() {
 		return supplierId;
@@ -59,8 +63,8 @@ public class Product {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public void setQuantityInStock(int quantityInStock) {
-		this.quantityInStock = quantityInStock;
+	public void setInStock(int inStock) {
+		this.inStock = inStock;
 	}
 	public void setSupplierId(int supplierId) {
 		this.supplierId = supplierId;
@@ -76,7 +80,7 @@ public class Product {
 			pst.setInt(1, this.id);
 			pst.setString(2, this.name);
 			pst.setInt(3, this.supplierId);
-			pst.setInt(4, this.quantityInStock);
+			pst.setInt(4, this.inStock);
 			pst.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -92,11 +96,12 @@ public class Product {
 		
 		try {
 			pst = con.prepareStatement("SELECT name,supplierId,inStock FROM product WHERE id = ?");
+			pst.setInt(1, id);
 			rst = pst.executeQuery();
 			if(rst.next()) {
 				product.name = rst.getString(1);
 				product.supplierId = rst.getInt(2);
-				product.quantityInStock = rst.getInt(3);
+				product.inStock = rst.getInt(3);
 				return product;
 			}
 		} catch (SQLException e) {
@@ -105,13 +110,127 @@ public class Product {
 		return null;	
 	}
 	
-	public boolean isProductSaved() {
+	public Product[] getAllProducts() {
+		Product product[] = new Product[getProductCount()];
+		PreparedStatement pst;
+		ResultSet rst;
+		
+		try {
+			pst = con.prepareStatement("SELECT id,name,supplierId,inStock FROM product");
+			rst = pst.executeQuery();
+			int i=0;
+			while(rst.next()) {
+				int id = rst.getInt(1);
+				String name = rst.getString(2);
+				int supplierId = rst.getInt(3);
+				int inStock = rst.getInt(4);
+				product[i] = new Product(id,name,supplierId,inStock);
+				i++;
+			}
+			return product;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	//getting details of products which  are supplied by particular supplier
+	public Product[] getAllProducts(int supplierIdIn) {
+		Product product[] = new Product[getProductCount(supplierIdIn)];
+		PreparedStatement pst;
+		ResultSet rst;
+		
+		try {
+			pst = con.prepareStatement("SELECT id,name,supplierId,inStock FROM product WHERE supplierId = ?");
+			pst.setInt(1, supplierIdIn);
+			rst = pst.executeQuery();
+			int i=0;
+			while(rst.next()) {
+				int id = rst.getInt(1);
+				String name = rst.getString(2);
+				int supplierId = rst.getInt(3);
+				int inStock = rst.getInt(4);
+				product[i] = new Product(id,name,supplierId,inStock);
+				System.out.println(product[i].toString());
+				i++;
+			}
+			return product;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", supplierId=" + supplierId + ", quantityInStock="
+				+ inStock + "]";
+	}
+
+	// 2d object Array for table population
+	public Object[][] populateProductTable() {
+		Product product[] = getAllProducts();
+		Object objects[][] = new Object[getProductCount()][3];
+		for (int i = 0; i < getProductCount(); i++) {
+			objects[i][0] = product[i].id;
+			objects[i][1] = product[i].name;
+			objects[i][2] = product[i].supplierId;
+			objects[i][3] = product[i].inStock;
+		}
+		return objects;
+	}	
+	
+	// getting number of product saved
+	public int getProductCount() {
+		connectToDatabase();
+		PreparedStatement pst;
+		ResultSet rst;
+
+		try {
+			pst = con.prepareStatement("SELECT COUNT(id) FROM product");
+			rst = pst.executeQuery();
+
+			if (rst.next()) {
+				return rst.getInt(1);
+			} else { // table empty
+				return 0;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public int getProductCount(int supplierId) {
+		connectToDatabase();
+		PreparedStatement pst;
+		ResultSet rst;
+
+		try {
+			pst = con.prepareStatement("SELECT COUNT(id) FROM product WHERE id = ?");
+			pst.setInt(1, supplierId);
+			rst = pst.executeQuery();
+
+			if (rst.next()) {
+				return rst.getInt(1);
+			} else { // table empty
+				return 0;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public boolean isSaved() {
 		connectToDatabase();
 		PreparedStatement pst;
 		ResultSet rst;
 		try {
-			pst = con.prepareStatement("SELECT * FROM CUSTOMER WHERE id = ?");
-			pst.setInt(1, id);
+			pst = con.prepareStatement("SELECT * FROM product WHERE id = ?");
+			pst.setInt(1, this.id);
 			rst = pst.executeQuery();
 			if(rst.next()) {
 				return true;
@@ -145,7 +264,7 @@ public class Product {
 	
 	
 	//establishing connection to database
-	private void connectToDatabase() {
+	protected void connectToDatabase() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/rsms","root","");
