554475618297f095b7fc87167471bb81c3544d07
MSatheem <138306393+MSatheem@users.noreply.github.com>
2024-09-27 14:42:58 +0530

    Return product UI added to admin Interface

--- RetailStoreManagementSystem/src/retailStore/AdminFunctionOption.java
+++ RetailStoreManagementSystem/src/retailStore/AdminFunctionOption.java
@@ -24,6 +24,7 @@ public class AdminFunctionOption extends JPanel {
 	private CustomerUI customerUI;
 	private StoreToShelfUI shelfUI;
 	private InvoiceUI invoiceUI;
+	private ReturnProductUI returnProductUI;
 	/**
 	 * Create the panel.
 	 */
@@ -35,6 +36,7 @@ public class AdminFunctionOption extends JPanel {
 		customerUI = null;
 		shelfUI = null;
 		invoiceUI = null;
+		returnProductUI = null;
 		panel.removeAll();
 		panel.setVisible(false);
 	}
@@ -74,6 +76,14 @@ public class AdminFunctionOption extends JPanel {
 		StoreMenu.add(mntmNewMenuItem);
 		
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Return");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menuSwitch();
+				returnProductUI = new ReturnProductUI();
+				panel.add(returnProductUI);
+				panel.setVisible(true);
+			}
+		});
 		mntmNewMenuItem_1.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		StoreMenu.add(mntmNewMenuItem_1);
 		
